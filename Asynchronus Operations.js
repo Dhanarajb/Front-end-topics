b23c38ca3ab25b4cd74a661e91d6b848b29e09c0
Dhanaraj <88299676+Dhanarajb@users.noreply.github.com>
2023-09-03 16:49:31 +0530

    Create Asynchronus Operations.js

---
+++ Asynchronus Operations.js
@@ -0,0 +1,19 @@
+JavaScript relies on asynchronous programming to prevent one slow task from making the whole application unresponsive. 
+  It's essential for building web apps that respond quickly to user actions, like button clicks. By using asynchronous techniques, 
+  developers can keep the app running smoothly, even when dealing with time-consuming tasks, ensuring a better user experience.
+
+1. Responsive: JavaScript is a popular programming language for building websites. It's often used to make things happen 
+  when you click buttons or submit forms on a webpage. Imagine if everything in JavaScript had to happen one after the other, 
+  like waiting in a long line. If something took a lot of time, like waiting for a slow computer, it would stop everything else from working, 
+  like freezing a computer screen. But with asynchronous code, it's like having a helper who can do things in the background while you keep using the computer. 
+  This way, the webpage can keep working even if something is taking a while to finish, so you can still click buttons and do stuff without the screen getting stuck.
+
+2. Non-blocking I/O: JavaScript is often used for tasks that involve I/O operations, like making HTTP requests or reading/writing to files. 
+  These operations can take time to complete, especially when interacting with external resources. Asynchronous programming allows you to 
+  initiate these operations and continue with other tasks while waiting for the I/O to complete, rather than blocking the execution of your code.
+
+3. Better user experience: Asynchronous code helps create a smoother and more enjoyable user experience. For example, when loading images, data, or resources, 
+    you can use asynchronous techniques to load them in the background, ensuring that the user can interact with the page without experiencing delays or hiccups.
+
+4. Callbacks, Promises, and Async/Await: JavaScript provides various ways to work with asynchronous code, such as callbacks, Promises, and the Async/Await syntax. 
+    These tools make it easier to manage asynchronous operations and handle errors, enhancing the readability and maintainability of your code.
