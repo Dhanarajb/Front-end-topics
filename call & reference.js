bfd05493bc3373c50f0ca91789eb9a6a511f6627
Dhanaraj <88299676+Dhanarajb@users.noreply.github.com>
2023-10-05 22:16:07 +0530

    Update call & reference.js

--- call & reference.js
+++ call & reference.js
@@ -1,10 +1,5 @@
-In JavaScript, understanding "pass by value" and "pass by reference" can sometimes be a bit tricky, as the terms themselves might not apply in the exact same way as in 
-  other programming languages. JavaScript uses a combination of these concepts when dealing with primitive types and reference types.
-
-Pass by Value:
-When you pass a primitive data type (such as a number, string, or boolean) to a function, a copy of the actual value is passed. This means that changes made to the 
-  parameter inside the function do not affect the original value outside the function.
-
+Pass by Value:⚙︎
+When you pass a primitive data type to a function, it creates a copy, so changes inside the function don't affect the original value.
   
 function modifyValue(x) {
   x = 10; // This modification does not affect the original value
@@ -13,9 +8,11 @@ function modifyValue(x) {
 let num = 5;
 modifyValue(num);
 console.log(num); // Output: 5
+
+⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘
+
 Pass by Reference (or Sharing):
-When you pass an object or array to a function, you are passing a reference to the memory location where the object or array is stored. 
-  This reference allows you to access and modify the same object or array inside the function, affecting the original object or array outside the function.
+When you pass an object or array to a function, you're passing a reference, so changes inside the function affect the original object or array outside of it.
 
   
 function modifyArray(arr) {
@@ -25,8 +22,11 @@ function modifyArray(arr) {
 const myArray = [1, 2, 3];
 modifyArray(myArray);
 console.log(myArray); // Output: [1, 2, 3, 4]
-It's important to note that JavaScript doesn't pass the reference itself but a copy of the reference. So, if you were to reassign the reference inside the function, 
-  it wouldn't affect the reference outside the function.
+
+⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘
+
+In JavaScript, when you pass an object or array to a function, you pass a copy of the reference, but if you reassign that reference inside the function, 
+it won't change the original reference outside the function.
 
 
 function reassignArray(arr) {
@@ -36,3 +36,73 @@ function reassignArray(arr) {
 const myArray = [1, 2, 3];
 reassignArray(myArray);
 console.log(myArray); // Output: [1, 2, 3]
+
+
+function reassignObject(obj) {
+    obj = { name: 'John' };
+}
+let person = { name: 'Alice' };
+reassignObject(person);
+console.log(person.name); // Output: Alice
+
+⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘
+
+What are the advantages of pass by value and pass by reference?
+
+Answer: Pass by value ensures that the original data remains unchanged, 
+        pass by reference allows you to work with and modify complex data structures efficiently.
+
+⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘
+
+How can you create a copy of an object in JavaScript to prevent modifying the original object?
+ChatGPT
+To create a copy of an object in JavaScript and prevent modifying the original object, you can use several methods, such as the spread operator ({...}), Object.assign(), or libraries like lodash. Here are examples of each method:
+
+Using the Spread Operator ({...}):
+
+
+const originalObject = { name: 'Alice', age: 30 };
+const copyObject = { ...originalObject };
+
+// Modify the copy
+copyObject.name = 'Bob';
+
+console.log(originalObject); // Output: { name: 'Alice', age: 30 }
+console.log(copyObject);    // Output: { name: 'Bob', age: 30 }
+Using Object.assign():
+
+
+const originalObject = { name: 'Alice', age: 30 };
+const copyObject = Object.assign({}, originalObject);
+
+// Modify the copy
+copyObject.name = 'Bob';
+
+console.log(originalObject); // Output: { name: 'Alice', age: 30 }
+console.log(copyObject);    // Output: { name: 'Bob', age: 30 }
+Using JSON.parse() and JSON.stringify() (Note: This method works well for simple objects, but it won't work with objects containing functions, undefined, or circular references):
+
+
+const originalObject = { name: 'Alice', age: 30 };
+const copyObject = JSON.parse(JSON.stringify(originalObject));
+
+// Modify the copy
+copyObject.name = 'Bob';
+
+console.log(originalObject); // Output: { name: 'Alice', age: 30 }
+console.log(copyObject);    // Output: { name: 'Bob', age: 30 }
+Using the lodash library:
+
+If you're working with more complex objects or need a deep copy, you can use the _.cloneDeep() function from the lodash library:
+
+
+const _ = require('lodash');
+
+const originalObject = { name: 'Alice', age: 30 };
+const copyObject = _.cloneDeep(originalObject);
+
+// Modify the copy
+copyObject.name = 'Bob';
+
+console.log(originalObject); // Output: { name: 'Alice', age: 30 }
+console.log(copyObject);    // Output: { name: 'Bob', age: 30 }
